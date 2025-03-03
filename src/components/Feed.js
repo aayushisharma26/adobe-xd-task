3b16f4badcc8654cbc59135948f4430f71a6a229
Aayushi Sharma <aayushisharma20@navgurukul.org>
2025-03-03 23:18:25 +0530

    Update the UI

--- src/components/Feed.js
+++ src/components/Feed.js
@@ -1,115 +1,3 @@
-// import React from "react";
-
-// const Feed = () => {
-//   return (
-//     <>
-      
-//       <div className="feed">
-        
-//       </div>
-//     </>
-//   );
-// };
-
-// export default Feed;
-
-
-// import React from "react";
-// import { FaHeart, FaComment, FaShare } from "react-icons/fa";
-
-// const posts = [
-//   {
-//     id: 1,
-//     name: "Lara Leones",
-//     username: "@thewallart",
-//     profileImage: "/assets/profile1.png",
-//     postImage: "/assets/post1.png",
-//     caption:
-//       "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.",
-//     likes: "9.8k",
-//     comments: "8.6k",
-//     shares: "7.2k",
-//   },
-//   {
-//     id: 2,
-//     name: "Thomas J.",
-//     username: "@thecustomcreater",
-//     profileImage: "/assets/profile2.png",
-//     postImage: "/assets/post2.png",
-//     caption:
-//       "Creativity is intelligence having fun. Bringing art to life!",
-//     likes: "5.2k",
-//     comments: "3.4k",
-//     shares: "1.9k",
-//   },
-// ];
-
-// const Feed = () => {
-//   return (
-//     <div className="flex justify-center">
-//       {/* Feed Container */}
-//       <div className="w-[600px] h-[600px] bg-white p-4 rounded-lg shadow-lg overflow-y-auto">
-//         {posts.map((post) => (
-//           <div key={post.id} className="bg-gray-100 p-4 rounded-lg mb-4">
-//             {/* User Info */}
-//             <div className="flex items-center space-x-3">
-//               <img
-//                 src={post.profileImage}
-//                 alt={post.name}
-//                 className="w-10 h-10 rounded-full"
-//               />
-//               <div>
-//                 <p className="font-semibold">{post.name}</p>
-//                 <p className="text-gray-500 text-sm">{post.username}</p>
-//               </div>
-//             </div>
-
-//             {/* Post Caption */}
-//             <p className="mt-3 text-gray-700">{post.caption}</p>
-
-//             {/* Post Image */}
-//             <img
-//               src={post.postImage}
-//               alt="Post"
-//               className="mt-3 w-full rounded-lg"
-//             />
-
-//             {/* Reactions */}
-//             <div className="flex justify-between items-center mt-3 text-gray-600">
-//               <div className="flex items-center space-x-2">
-//                 <FaHeart className="text-red-500" />
-//                 <span>{post.likes}</span>
-//               </div>
-//               <div className="flex items-center space-x-2">
-//                 <FaComment />
-//                 <span>{post.comments}</span>
-//               </div>
-//               <div className="flex items-center space-x-2">
-//                 <FaShare />
-//                 <span>{post.shares}</span>
-//               </div>
-//             </div>
-//           </div>
-//         ))}
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default Feed;
-
-
-
-
-
-
-
-
-
-
-
-
-
 import React from "react";
 
 const posts = [
@@ -118,7 +6,7 @@ const posts = [
     user: "Lara Leones",
     username: "@thewallart",
     text: "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.",
-    image: "/assets/profile1.png",
+    image: "/assets/post1.png",
     likeIcon: "/assets/like.png",
     commentIcon: "/assets/comment.png",
     shareIcon: "/assets/share.png",
@@ -131,7 +19,7 @@ const posts = [
     user: "Thomas J.",
     username: "@thecustomcreater",
     text: "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.",
-    image: "/assets/post1.png",
+    image: "/assets/post2.png",
     likeIcon: "/assets/like.png",
     commentIcon: "/assets/comment.png",
     shareIcon: "/assets/share.png",
@@ -148,7 +36,7 @@ const Feed = () => {
         <div className="feed" key={post.id}>
           {/* Header */}
           <div className="post-header">
-            <img src="/assets/post1.png" alt="user" className="avatar" />
+            <img src="/assets/profile1.png" alt="user" className="avatar" />
             <div className="user-info">
               <h4>{post.user}</h4>
               <p>{post.username}</p>
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,25 +1,65 @@
+// import React from "react";
+
+// const Header = () => {
+//   return (
+//     <>
+//       <div>
+
+
+//       </div>
+//       <div className="logo">
+//         <div className="logo-dun">LOGO</div>
+//       </div>
+
+//       <div className="input">
+//         <div className="search-icon">
+//           <img
+//             src="https://static.vecteezy.com/system/resources/thumbnails/020/591/799/small/search-icon-with-outline-style-free-vector.jpg"
+//             alt="Search"
+//           />
+//         </div>
+//         <input type="text" placeholder="Search..." className="search-bar" />
+//       </div>
+
+//       <div className="headerrightside">
+//         <h1>Become a Seller</h1>
+//       </div>
+//     </>
+//   );
+// };
+
+// export default Header;
+
+
+
+
 import React from "react";
 
 const Header = () => {
   return (
     <>
-      <div className="logo">
-        <div className="logo-dun">LOGO</div>
-      </div>
-
-      <div className="input">
-        <div className="search-icon">
-          <img
-            src="https://static.vecteezy.com/system/resources/thumbnails/020/591/799/small/search-icon-with-outline-style-free-vector.jpg"
-            alt="Search"
-          />
+      <div className="header">
+        <div className="logo">
+          <div className="logo-dun">LOGO</div>
         </div>
-        <input type="text" placeholder="Search..." className="search-bar" />
-      </div>
 
-      <div className="headerrightside">
-        <h1>Become a Seller</h1>
+        <div className="input">
+          <div className="search-icon">
+            <img
+              src="https://static.vecteezy.com/system/resources/thumbnails/020/591/799/small/search-icon-with-outline-style-free-vector.jpg"
+              alt="Search"
+            />
+          </div>
+          <input type="text" placeholder="Search..." className="search-bar" />
+        </div>
+
+        <div className="headerrightside">
+          <h1>Become a Seller</h1>
+        </div>
+
+
       </div>
+      
     </>
   );
 };
