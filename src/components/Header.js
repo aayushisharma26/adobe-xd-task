aec3e77054359d26fcc6c2d314456b6340ae3158
Aayushi Sharma <aayushisharma20@navgurukul.org>
2025-03-03 23:45:13 +0530

    Added Icons

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,46 +1,11 @@
-// import React from "react";
-
-// const Header = () => {
-//   return (
-//     <>
-//       <div>
-
-
-//       </div>
-//       <div className="logo">
-//         <div className="logo-dun">LOGO</div>
-//       </div>
-
-//       <div className="input">
-//         <div className="search-icon">
-//           <img
-//             src="https://static.vecteezy.com/system/resources/thumbnails/020/591/799/small/search-icon-with-outline-style-free-vector.jpg"
-//             alt="Search"
-//           />
-//         </div>
-//         <input type="text" placeholder="Search..." className="search-bar" />
-//       </div>
-
-//       <div className="headerrightside">
-//         <h1>Become a Seller</h1>
-//       </div>
-//     </>
-//   );
-// };
-
-// export default Header;
-
-
-
-
 import React from "react";
 
 const Header = () => {
   return (
     <>
       <div className="header">
-        <div className="logo">
-          <div className="logo-dun">LOGO</div>
+        <div className="logo">LOGO
+          
         </div>
 
         <div className="input">
